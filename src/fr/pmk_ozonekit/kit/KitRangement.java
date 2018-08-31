8eec616050c5f8796d047fc907334684ce56faf3
Sufike <l.christ54260@gmail.com>
2018-08-31 02:34:41 +0200

    Fix ISubCommand On Kit

--- src/fr/pmk_ozonekit/kit/KitRangement.java
+++ src/fr/pmk_ozonekit/kit/KitRangement.java
@@ -1,16 +1,28 @@
 package fr.pmk_ozonekit.kit;
 
+import java.util.List;
+
 import org.bukkit.Material;
+import org.bukkit.command.Command;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.material.*;
 
-public class KitRangement {
+import fr.pmk_ozonekit.commands.ISubCommand;
+
+public class KitRangement implements ISubCommand {
 	
 	public void onCommand(Player p) {
 	
 		
 	
+	}
+
+	@Override
+	public boolean onSubCommand(Player sender, Command cmd, List<String> args) {
+		
+		return false;
+		
 	}
 }
