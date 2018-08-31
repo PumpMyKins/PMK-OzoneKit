e92cf231fcd5ce4574cd98ae7402a05ba234d3ac
Sufike <l.christ54260@gmail.com>
2018-08-31 02:37:18 +0200

    Add Getter and Setter to the manager

--- src/fr/pmk_ozonekit/Main/KitManager.java
+++ src/fr/pmk_ozonekit/Main/KitManager.java
@@ -1,6 +1,5 @@
 package fr.pmk_ozonekit.Main;
 
-import fr.pmk_ozonekit.commands.ISubCommand;
 import fr.pmk_ozonekit.commands.kitCommandExecutor;
 import fr.pmk_ozonekit.commands.kitHelpCommand;
 import fr.pmk_ozonekit.kit.KitRangement;
@@ -17,5 +16,15 @@ public class KitManager {
 		
 		kitCmd.addSubCommand("help", new kitHelpCommand());
 		kitCmd.addSubCommand("rangement", new KitRangement());
+		
+		
+	}
+
+	public kitCommandExecutor getKitCmd() {
+		return kitCmd;
+	}
+
+	public void setKitCmd(kitCommandExecutor kitCmd) {
+		this.kitCmd = kitCmd;
 	}
 }
