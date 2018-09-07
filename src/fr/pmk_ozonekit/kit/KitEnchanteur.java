7baefc8d8b20474b9491caf834609475d4c84d18
Sufike <l.christ54260@gmail.com>
2018-09-07 22:08:12 +0200

    kit enchant

--- src/fr/pmk_ozonekit/kit/KitEnchanteur.java
+++ src/fr/pmk_ozonekit/kit/KitEnchanteur.java
@@ -6,6 +6,7 @@ import org.bukkit.command.Command;
 import org.bukkit.entity.Player;
 
 import fr.pmk_ozonekit.commands.ISubCommand;
+import fr.pmk_ozonekit.utils.ItemGiver;
 import fr.pmk_ozonekit.utils.KitChecker;
 
 public class KitEnchanteur implements ISubCommand{
@@ -13,9 +14,28 @@ public class KitEnchanteur implements ISubCommand{
 	@Override
 	public boolean onSubCommand(Player sender, Command cmd, List<String> args) {
 		
+		int disenchantementTable = 1433;
+		int experienceObelisk = 1526;
+		int typesettingTable = 920;
+		int monocle = 4544; //2
+		int printingPress = 921;
+		int printingPressChase = 4540;
+		int inkSac = 351;
+		
 		if(KitChecker.onCheck(sender, "enchantement") == true) {
 			
+			int xp = sender.getTotalExperience();
 			sender.sendMessage("Voici votre kit :");
+			ItemGiver.giveItem(sender, disenchantementTable, 1);
+			ItemGiver.giveItem(sender, experienceObelisk, 1);
+			ItemGiver.giveItem(sender, typesettingTable, 1);
+			ItemGiver.giveItem(sender, monocle, 1);
+			ItemGiver.giveItem(sender, printingPress, 1);
+			ItemGiver.giveItem(sender, printingPressChase, 1);
+			ItemGiver.giveItem(sender, inkSac, 64);
+			
+			sender.setTotalExperience(xp + 29315);
+			
 		}
 		
 		return false;
