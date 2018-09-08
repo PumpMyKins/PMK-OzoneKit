81313c3d343b9c98f6a096033b02fa64f459f681
Sufike <l.christ54260@gmail.com>
2018-09-08 09:48:52 +0200

    Corriger les parenthèse chelou

--- src/fr/pmk_ozonekit/commands/KitHelpCommand.java
+++ src/fr/pmk_ozonekit/commands/KitHelpCommand.java
@@ -8,7 +8,7 @@ import org.bukkit.entity.Player;
 public class KitHelpCommand implements ISubCommand {
 	public boolean onSubCommand(Player sender, Command cmd, List<String> args) {
 		
-		sender.sendMessage("§6§l[§r§2PumpMyKit§6§l]§r§2§oListe des Commandes §r6§l[§r§2PumpMyKit§6§l]");
+		sender.sendMessage("§6§l[§r§2PumpMyKit§6§l]§r§2§oListe des Commandes §r§6§l[§r§2PumpMyKit§6§l]");
 		sender.sendMessage("§2 /kit list");
 		return true;
 	}
