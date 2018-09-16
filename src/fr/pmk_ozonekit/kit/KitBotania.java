6e112534809b02ef9ec3a2af89f2719a1f3485cc
Sufike <l.christ54260@gmail.com>
2018-09-16 10:06:23 +0200

    add OpenInv Botania

--- src/fr/pmk_ozonekit/kit/KitBotania.java
+++ src/fr/pmk_ozonekit/kit/KitBotania.java
@@ -2,12 +2,15 @@ package fr.pmk_ozonekit.kit;
 
 import java.util.List;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
 
 import fr.pmk_ozonekit.commands.ISubCommand;
 import fr.pmk_ozonekit.utils.ItemGiver;
 import fr.pmk_ozonekit.utils.KitChecker;
+import net.md_5.bungee.api.ChatColor;
 
 public class KitBotania implements ISubCommand{
 	
@@ -20,18 +23,21 @@ public class KitBotania implements ISubCommand{
 		int floralFertiliser = 4351;
 		int blackerLotus = 4434; //1
 		int overgrowthSeeds = 4442;
-		
+		String kitname ="Botania";
 		
 		if(KitChecker.onCheck(sender, "botania") == true) {
 			
-			sender.sendMessage("Voici votre kit Botania :");
-			ItemGiver.giveItem(sender, manaPool, 1);
-			ItemGiver.giveItem(sender, manaSpreader, 2);
-			ItemGiver.giveItem(sender, velocityLens, 2, 1);
-			ItemGiver.giveItem(sender, floralFertiliser, 64);
-			ItemGiver.giveItem(sender, blackerLotus, 16, 1);
-			ItemGiver.giveItem(sender, overgrowthSeeds, 8);
+			Inventory invKit = Bukkit.createInventory(null, 9, ChatColor.RED + "" + ChatColor.BOLD + kitname);
 			
+			sender.sendMessage("Voici votre kit Botania :");
+			ItemGiver.giveItem(sender, manaPool, 1, invKit);
+			ItemGiver.giveItem(sender, manaSpreader, 2, invKit);
+			ItemGiver.giveItem(sender, velocityLens, 2, 1, invKit);
+			ItemGiver.giveItem(sender, floralFertiliser, 64, invKit);
+			ItemGiver.giveItem(sender, blackerLotus, 16, 1, invKit);
+			ItemGiver.giveItem(sender, overgrowthSeeds, 8, invKit);
+		
+			sender.openInventory(invKit);
 		}
 		
 		return false;
