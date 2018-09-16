4faf767e1c56f2b9fefcf2c54bed5b2a20c5d918
Sufike <l.christ54260@gmail.com>
2018-09-16 10:13:57 +0200

    OpenInv finish push

--- src/fr/pmk_ozonekit/kit/KitRangement.java
+++ src/fr/pmk_ozonekit/kit/KitRangement.java
@@ -2,12 +2,15 @@ package fr.pmk_ozonekit.kit;
 
 import java.util.List;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
 
 import fr.pmk_ozonekit.commands.ISubCommand;
 import fr.pmk_ozonekit.utils.ItemGiver;
 import fr.pmk_ozonekit.utils.KitChecker;
+import net.md_5.bungee.api.ChatColor;
 
 public class KitRangement implements ISubCommand {
 	
@@ -20,16 +23,21 @@ public class KitRangement implements ISubCommand {
 		int enderChest = 3055;
 		int enderTank = 3055; //4096
 		int alchemicalBag = 6730;
+		String kitname ="Rangement";
 		
 		if(KitChecker.onCheck(sender, "rangement") == true) {
 			
+			Inventory invKit = Bukkit.createInventory(null, 8, ChatColor.RED + "" + ChatColor.BOLD + kitname);
+			
 			sender.sendMessage("Voici votre kit Rangement :");
-			ItemGiver.giveItem(sender, drawers, 16);
-			ItemGiver.giveItem(sender, diamondDrawerUpgrade, 16, 5);
-			ItemGiver.giveItem(sender, compressedChest, 4);
-			ItemGiver.giveItem(sender, enderChest, 2);
-			ItemGiver.giveItem(sender, enderTank, 2, 4096);
-			ItemGiver.giveItem(sender, alchemicalBag, 2);
+			ItemGiver.giveItem(sender, drawers, 16, invKit);
+			ItemGiver.giveItem(sender, diamondDrawerUpgrade, 16, 5, invKit);
+			ItemGiver.giveItem(sender, compressedChest, 4, invKit);
+			ItemGiver.giveItem(sender, enderChest, 2, invKit);
+			ItemGiver.giveItem(sender, enderTank, 2, 4096, invKit);
+			ItemGiver.giveItem(sender, alchemicalBag, 2, invKit);
+			
+			sender.openInventory(invKit);
 			
 		}
 		
--- src/fr/pmk_ozonekit/kit/KitTinker.java
+++ src/fr/pmk_ozonekit/kit/KitTinker.java
@@ -2,12 +2,15 @@ package fr.pmk_ozonekit.kit;
 
 import java.util.List;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
 
 import fr.pmk_ozonekit.commands.ISubCommand;
 import fr.pmk_ozonekit.utils.ItemGiver;
 import fr.pmk_ozonekit.utils.KitChecker;
+import net.md_5.bungee.api.ChatColor;
 
 public class KitTinker implements ISubCommand{
 
@@ -20,17 +23,20 @@ public class KitTinker implements ISubCommand{
 		int smelteryTank = 1735;
 		int smelteryController = 1733;
 		int searedFaucet =1737; //1
-		
+		String kitname ="Kit Tinker";
 		if(KitChecker.onCheck(sender, "tinker") == true) {
 			
-			sender.sendMessage("Voici votre kit Tinker :");
-			ItemGiver.giveItem(sender, glowstoneIngot, 16, 3);
-			ItemGiver.giveItem(sender, searedBricks, 32, 2);
-			ItemGiver.giveItem(sender, smelteryDrain, 4, 1);
-			ItemGiver.giveItem(sender, smelteryTank, 1);
-			ItemGiver.giveItem(sender, smelteryController, 1);
-			ItemGiver.giveItem(sender, searedFaucet, 4, 1);
+			Inventory invKit = Bukkit.createInventory(null, 8, ChatColor.RED + "" + ChatColor.BOLD + kitname);
 			
+			sender.sendMessage("Voici votre kit Tinker :");
+			ItemGiver.giveItem(sender, glowstoneIngot, 16, 3, invKit);
+			ItemGiver.giveItem(sender, searedBricks, 32, 2, invKit);
+			ItemGiver.giveItem(sender, smelteryDrain, 4, 1, invKit);
+			ItemGiver.giveItem(sender, smelteryTank, 1, invKit);
+			ItemGiver.giveItem(sender, smelteryController, 1, invKit);
+			ItemGiver.giveItem(sender, searedFaucet, 4, 1, invKit);
+			
+			sender.openInventory(invKit);
 		}
 		
 		return false;
