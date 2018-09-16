3b9b5db34f80b1f578905c9597eaa70a8726c891
Sufike <l.christ54260@gmail.com>
2018-09-16 10:00:52 +0200

    openInv vegan

--- src/fr/pmk_ozonekit/kit/KitVegan.java
+++ src/fr/pmk_ozonekit/kit/KitVegan.java
@@ -2,12 +2,15 @@ package fr.pmk_ozonekit.kit;
 
 import java.util.List;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
 
 import fr.pmk_ozonekit.commands.ISubCommand;
 import fr.pmk_ozonekit.utils.ItemGiver;
 import fr.pmk_ozonekit.utils.KitChecker;
+import net.md_5.bungee.api.ChatColor;
 
 public class KitVegan implements ISubCommand{
 	
@@ -21,18 +24,21 @@ public class KitVegan implements ISubCommand{
 		int harvestcraftOldworldveggiesoupItem = 6141; 
 		int harvestcraftVeggiestripsItem = 6343; 
 		int harvestcraftRoastedrootveggiemedleyItem = 5958; 
-		
+		String kitname = "Vegan";
 		if(KitChecker.onCheck(sender, "vegan") == true) {
 			
 			sender.sendMessage("Voici votre kit vegan :");
 			
-			ItemGiver.giveItem(sender, harvestcraftMixedsaladItem, 4);
-			ItemGiver.giveItem(sender, harvestcraftFruitsaladItem, 2);
-			ItemGiver.giveItem(sender, NaturaBerryMedley, 6);
-			ItemGiver.giveItem(sender, harvestcraftVeggiestirfryItem, 3);
-			ItemGiver.giveItem(sender, harvestcraftOldworldveggiesoupItem, 2);
-			ItemGiver.giveItem(sender, harvestcraftVeggiestripsItem, 4);
-			ItemGiver.giveItem(sender, harvestcraftRoastedrootveggiemedleyItem, 4);
+			Inventory invKit = Bukkit.createInventory(null, 9, ChatColor.RED + "" + ChatColor.BOLD + kitname);
+			
+			ItemGiver.giveItem(sender, harvestcraftMixedsaladItem, 4, invKit);
+			ItemGiver.giveItem(sender, harvestcraftFruitsaladItem, 2, invKit);
+			ItemGiver.giveItem(sender, NaturaBerryMedley, 6, invKit);
+			ItemGiver.giveItem(sender, harvestcraftVeggiestirfryItem, 3, invKit);
+			ItemGiver.giveItem(sender, harvestcraftOldworldveggiesoupItem, 2, invKit);
+			ItemGiver.giveItem(sender, harvestcraftVeggiestripsItem, 4, invKit);
+			ItemGiver.giveItem(sender, harvestcraftRoastedrootveggiemedleyItem, 4, invKit);
+			ItemGiver.giveItem(sender, 31, 32, 1, invKit);
 			
 			
 		}
