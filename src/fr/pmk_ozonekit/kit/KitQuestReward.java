0e7c70c85438396041600d73adbebcb22238ff03
Sufike <l.christ54260@gmail.com>
2018-09-16 10:12:04 +0200

    OpenInv QR

--- src/fr/pmk_ozonekit/kit/KitQuestReward.java
+++ src/fr/pmk_ozonekit/kit/KitQuestReward.java
@@ -2,12 +2,15 @@ package fr.pmk_ozonekit.kit;
 
 import java.util.List;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
 
 import fr.pmk_ozonekit.commands.ISubCommand;
 import fr.pmk_ozonekit.utils.ItemGiver;
 import fr.pmk_ozonekit.utils.KitChecker;
+import net.md_5.bungee.api.ChatColor;
 
 public class KitQuestReward implements ISubCommand{
 	
@@ -15,11 +18,16 @@ public class KitQuestReward implements ISubCommand{
 	public boolean onSubCommand(Player sender, Command cmd, List<String> args) {
 		
 		int lootChests = 7569;
+		String kitname ="QuestReward";
 		
 		if(KitChecker.onCheck(sender, "questreward") == true) {
 			
+			Inventory invKit = Bukkit.createInventory(null, 8, ChatColor.RED + "" + ChatColor.BOLD + kitname);
+			
 			sender.sendMessage("Voici votre kit QuestReward :");
-			ItemGiver.giveItem(sender, lootChests, 16);
+			ItemGiver.giveItem(sender, lootChests, 16, invKit);
+			
+			sender.openInventory(invKit);
 		}
 		
 		return false;
